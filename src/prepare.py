fc966aa74b996eacd7fa8a67403fad108eefd1b9
Abdullah <abdullahkaled01111@gmail.com>
2024-08-27 12:26:06 +0300

    improvements

--- src/prepare.py
+++ src/prepare.py
@@ -28,9 +28,9 @@ def save_df(df, output_dir, filename="prepared_data.csv"):
 
 
 def main():
-    if len(sys.argv) != 2:
-        sys.stderr.write("Arguments error. Usage: python script.py <input_data_path>\n")
-        sys.exit(1)
+    # if len(sys.argv) != 2:
+    #     sys.stderr.write("Arguments error. Usage: python script.py <input_data_path>\n")
+    #     sys.exit(1)
 
     in_path = sys.argv[1]
     data_input = os.path.join(in_path, "data.csv")
